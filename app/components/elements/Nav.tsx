a5064e4427c9da965791812989fdd2a8a0bea1d9
aMand7 <amanddseventaurus@gmail.com>
2023-08-23 21:13:26 +0700

    Fixed: memperbaiki tampilan dan menambahkan animasi pada navigasi mobile

--- app/components/elements/Nav.tsx
+++ app/components/elements/Nav.tsx
@@ -1,7 +1,7 @@
 
 const Nav = ({children}: {children: React.ReactNode}) => {
   return (
-    <nav className='w-screen h-10 bg-primary grid grid-cols-3 items-center fixed top-0 z-30'>{children}</nav>
+    <nav className='w-screen h-10 bg-primary grid grid-cols-3 items-center fixed top-0 z-20'>{children}</nav>
   )
 }
 
--- app/components/fragments/Menu.tsx
+++ app/components/fragments/Menu.tsx
@@ -2,6 +2,8 @@
 import { HamburgerIcon } from '@chakra-ui/icons'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState, useEffect } from "react"
+import { usePathname } from "next/navigation"
 import {
   Menu,
   MenuButton,
@@ -10,28 +12,49 @@ import {
 } from '@chakra-ui/react'
 
 const HamburgerMenu = () => {
+  const [currentPath, setCurrenPath] = useState('')
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setCurrenPath(pathname.substring(8))
+  }, [pathname])
 
   return (
-    <div className='md:hidden justify-self-center'>
+    <div className='md:hidden justify-self-center z-50'>
     <Menu>
-        <MenuButton className='hover:bg-slate-400 rounded-md'>
+        <MenuButton>
           <HamburgerIcon boxSize={6}/>
         </MenuButton>
-      <MenuList>
+      <MenuList className='z-50'>
           <Link href='/movies/popular'>
-            <MenuItem>Popular<Image src='/img/like.svg' alt='like-icon' width={20} height={20} className='ml-auto mr-0' /></MenuItem>
+            <MenuItem>
+              Popular
+              <Image src='/img/like.svg' alt='like-icon' width={20} height={20} className={`ml-auto mr-0 ${currentPath === 'popular' && 'animate-bounce'}`} />
+            </MenuItem>
           </Link>
         <Link href='/movies/trending'>
-          <MenuItem>Trending<Image src='/img/trending.svg' alt='trending-icon' width={20} height={20} className='ml-auto mr-0' /></MenuItem>
+            <MenuItem>
+              Trending
+              <Image src='/img/trending.svg' alt='trending-icon' width={20} height={20} className={`ml-auto mr-0 ${currentPath === 'trending' && 'animate-bounce'}`} />
+              </MenuItem>
           </Link>
         <Link href='/movies/nowplaying'>
-          <MenuItem>Now Playing<Image src='/img/now-playing.svg' alt='now-playing-icon' width={20} height={20} className='ml-auto mr-0' /></MenuItem>
+            <MenuItem>
+              Now Playing
+              <Image src='/img/now-playing.svg' alt='now-playing-icon' width={20} height={20} className={`ml-auto mr-0 ${currentPath === 'nowplaying' && 'animate-bounce'}`} />
+              </MenuItem>
           </Link>
         <Link href='/movies/upcoming'>
-            <MenuItem>Upcoming<Image src='/img/upcoming.svg' alt='upcoming-icon' width={20} height={20} className='ml-auto mr-0' /></MenuItem>
+            <MenuItem>
+              Upcoming
+              <Image src='/img/upcoming.svg' alt='upcoming-icon' width={20} height={20} className={`ml-auto mr-0 ${currentPath === 'upcoming' && 'animate-bounce'}`} />
+              </MenuItem>
           </Link>
         <Link href='/movies/toprated'>
-          <MenuItem>Top Rated<Image src='/img/top-rated.svg' alt='top-rated-icon' width={20} height={20} className='ml-auto mr-0' /></MenuItem>
+          <MenuItem>
+          Top Rated
+              <Image src='/img/top-rated.svg' alt='top-rated-icon' width={20} height={20} className={`ml-auto mr-0 ${currentPath === 'toprated' && 'animate-bounce'}`} />
+            </MenuItem>
           </Link>
       </MenuList>
     </Menu>
