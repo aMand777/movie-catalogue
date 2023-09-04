6556c6f5a24421d391e6f96e6364fc8260501bb3
aMand7 <amanddseventaurus@gmail.com>
2023-09-04 12:37:50 +0700

    FEATURE: Menambah animasi hover pada button halaman home

--- app/page.tsx
+++ app/page.tsx
@@ -32,32 +32,42 @@ export default function Home() {
   return (
     <>
       <main className='container mx-auto mt-28 -z-10'>
-        <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full mb-2 bg-primary ${popularMovies.length > 0 ? 'block' : 'hidden'}`}><Link href='/movies/popular'>Popular</Link></div>
+        <Link href='/movies/popular'>
+          <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full mb-2 bg-primary hover:bg-red-600 ${popularMovies.length > 0 ? 'block' : 'hidden'}`}>Popular</div>
+        </Link>
         <div className='scroll-none flex overflow-scroll mx-1 md:mx-3'>
       {popularMovies.map((movie: any) => (
       <CardMoviesHome key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
         ))}
         </div>
-        <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary ${trendingMovies.length > 0 ? 'block' : 'hidden'}`}><Link href='/movies/trending'>Trending</Link></div>
+        <Link href='/movies/trending'>
+          <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary hover:bg-red-600 ${trendingMovies.length > 0 ? 'block' : 'hidden'}`}>Trending</div>
+        </Link>
         <div className='scroll-none flex overflow-scroll mx-1 md:mx-3'>
       {trendingMovies.map((movie: any) => (
       <CardMoviesHome key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
         ))}
         </div>
-        <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary ${nowPlayingMovies.length > 0 ? 'block' : 'hidden'}`}><Link href='/movies/nowplaying'>Now Playing</Link></div>
+        <Link href='/movies/nowplaying'>
+          <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary hover:bg-red-600 ${nowPlayingMovies.length > 0 ? 'block' : 'hidden'}`}>Now Playing</div>
+        </Link>
         <div className='scroll-none flex overflow-scroll mx-1 md:mx-3'>
       {nowPlayingMovies.map((movie: any) => (
       <CardMoviesHome key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
         ))}
         </div>
-        <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary ${upcomingMovies.length > 0 ? 'block' : 'hidden'}`}><Link href='/movies/upcoming'>Upcoming</Link></div>
+        <Link href='/movies/upcoming'>
+          <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary hover:bg-red-600 ${upcomingMovies.length > 0 ? 'block' : 'hidden'}`}>Upcoming</div>
+        </Link>
         <div className='scroll-none flex overflow-scroll mx-1 md:mx-3'>
       {upcomingMovies.map((movie: any) => (
       <CardMoviesHome key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
         ))}
         </div>
-        <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary ${topRatedMovies.length > 0 ? 'block' : 'hidden'}`}><Link href='/movies/toprated'>Top Rated</Link></div>
-        <div className='scroll-none flex overflow-scroll mx-1 md:mx-3'>
+        <Link href='/movies/toprated'>
+          <div className={`text-white font-semibold italic text-xs mx-1 md:mx-3 w-fit p-1 rounded-b-full rounded-tr-full my-2 bg-primary hover:bg-red-600 ${topRatedMovies.length > 0 ? 'block' : 'hidden'}`}>Top Rated</div>
+        </Link>
+        <div className='scroll-none flex overflow-scroll mx-1 md:mx-3 mb-10'>
       {topRatedMovies.map((movie: any) => (
       <CardMoviesHome key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} />
         ))}
