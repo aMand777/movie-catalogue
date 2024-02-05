0b5214aa26f76007e6ae31fb8385827216df3ea0
aMand7 <amanddseventaurus@gmail.com>
2024-02-05 22:28:10 +0700

    feat: add package husky & implemented loading skeleton

--- app/movies/toprated/page.tsx
+++ app/movies/toprated/page.tsx
@@ -3,6 +3,7 @@ import { getTopRatedMovies } from '@/app/services/movies'
 import { useEffect, useState } from 'react'
 import CardMovie from '../../components/fragments/CardMovie'
 import Pagination from '@/app/components/fragments/Pagination'
+import CardMovieSkeleton from '@/app/components/fragments/CardMovieSkeleton'
 
 const TopRatedPage = () => {
   const [movies, setMovies] = useState([])
@@ -33,12 +34,27 @@ const TopRatedPage = () => {
 
   return (
     <>
-    <div className='grid grid-cols-3 gap-2 px-3 md:px-5 lg:px-10 md:grid-cols-4 md:gap-3 lg:grid-cols-5 2xl:grid-cols-10 lg:gap-5 top-28 absolute -z-10'>
-      {movies.length > 0 && movies.map((movie: MovieInterface) => (
-      <CardMovie key={movie.id} id={movie.id} title={movie.title} poster={movie.poster_path} rating={ movie.vote_average } />
-      ))}
-        <Pagination totalPage={totalPages} currentPage={page} nextPage={nextPage} prevPage={prevPage} />
-    </div>
+      <div className='grid grid-cols-3 gap-2 px-3 md:px-5 lg:px-10 md:grid-cols-4 md:gap-3 lg:grid-cols-5 2xl:grid-cols-10 lg:gap-5 top-28 left-0 right-0 absolute -z-10'>
+        {movies.length > 0 ? (
+          movies.map((movie: MovieInterface) => (
+            <CardMovie
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              poster={movie.poster_path}
+              rating={movie.vote_average}
+            />
+          ))
+        ) : (
+          <CardMovieSkeleton loop={20} />
+        )}
+        <Pagination
+          totalPage={totalPages}
+          currentPage={page}
+          nextPage={nextPage}
+          prevPage={prevPage}
+        />
+      </div>
     </>
   )
 }
