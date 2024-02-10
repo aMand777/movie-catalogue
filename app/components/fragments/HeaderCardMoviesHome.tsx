fc88e51560356a2f5489396d4a516ad740397ee6
aMand7 <amanddseventaurus@gmail.com>
2024-02-10 23:40:51 +0700

    debug: fixed navbar component

--- app/components/fragments/HeaderCardMoviesHome.tsx
+++ app/components/fragments/HeaderCardMoviesHome.tsx
@@ -14,9 +14,9 @@ const HeaderCardMoviesHome: React.FC<HeaderCardMoviesHomeProps> = ({ children, t
   return (
     <>
     <div className='flex justify-between items-center mx-1 md:mx-3 mt-5'>
-      <Badge variant='solid' colorScheme='red' className='rounded-tr-lg cursor-default'>{children}</Badge>
+      <Badge variant='solid' colorScheme='red' className='rounded-tr-lg cursor-default text-sm'>{children}</Badge>
       <Link href={link}>
-        <span className='flex text-viewAll text-[10px] lg:text-xs hover:text-white items-center'>
+        <span className='flex text-viewAll text-xs lg:text-sm hover:text-white items-center'>
           <p className='hover:mr-1 p-2 duration-500'>{title}</p>
           <ArrowForwardIcon boxSize={4} className='hover:ml-1 duration-500' />
         </span>
